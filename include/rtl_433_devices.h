5cbedfad23d6ba21f47e9b4cdccb394d642d68f1
Northern Man <sgracey@Heisenberg.local>
2021-04-03 13:25:55 -0400

    FSK_PULSE_PCM devices added to update

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -19,6 +19,8 @@
   DECL(akhan_100F14) \
   DECL(alectov1) \
   DECL(ambientweather_tx8300) \
+  DECL(ambientweather_wh31e) \
+  DECL(archos_tbh) \
   DECL(auriol_afw2a1) \
   DECL(auriol_ahfl) \
   DECL(auriol_hg02832) \
@@ -26,27 +28,41 @@
   DECL(blyss) \
   DECL(brennenstuhl_rcs_2044) \
   DECL(bresser_3ch) \
+  DECL(bresser_5in1) \
+  DECL(bresser_6in1) \
+  DECL(bresser_7in1) \
   DECL(bt_rain) \
   DECL(burnhardbbq) \
   DECL(calibeur_RF104) \
   DECL(cardin) \
+  DECL(cavius) \
   DECL(chuango) \
   DECL(companion_wtr001) \
+  DECL(current_cost) \
+  DECL(danfoss_CFR) \
   DECL(digitech_xc0324) \
+  DECL(directv) \
   DECL(dish_remote_6_3) \
+  DECL(ecodhome) \
   DECL(ecowitt) \
   DECL(eurochron_efth800) \
   DECL(elro_db286a) \
   DECL(elv_em1000) \
   DECL(elv_ws2000) \
+  DECL(emontx) \
+  DECL(esic_emt7110) \
   DECL(esperanza_ews) \
   DECL(eurochron) \
   DECL(fineoffset_WH2) \
+  DECL(fineoffset_WH25) \
+  DECL(fineoffset_WH51) \
   DECL(fineoffset_WH0530) \
   DECL(fineoffset_wh1050) \
   DECL(fineoffset_wh1080) \
+  DECL(fineoffset_wh1080_fsk) \
   DECL(fs20) \
   DECL(ft004b) \
+  DECL(ge_coloreffects) \
   DECL(generic_motion) \
   DECL(generic_remote) \
   DECL(generic_temperature_sensor) \
@@ -54,34 +70,45 @@
   DECL(gt_wt_02) \
   DECL(gt_wt_03) \
   DECL(hcs200) \
+  DECL(holman_ws5029pcm) \
   DECL(honeywell_wdb) \
   DECL(ht680) \
+  DECL(ikea_sparsnas) \
   DECL(infactory) \
   DECL(kw9015b) \
+  DECL(insteon) \
   DECL(interlogix) \
   DECL(intertechno) \
   DECL(kedsum) \
   DECL(kerui) \
   DECL(lacrossetx) \
+  DECL(lacrosse_breezepro) \
+  DECL(lacrosse_r1) \
+  DECL(lacrosse_th3) \
   DECL(lacrosse_tx141x) \
+  DECL(lacrosse_tx29) \
+  DECL(lacrosse_tx35) \
+  DECL(lacrosse_wr1) \
   DECL(lacrosse_ws7000) \
   DECL(lacrossews) \
   DECL(lightwave_rf) \
+  DECL(m_bus_mode_c_t) \
+  DECL(m_bus_mode_s) \
+  DECL(m_bus_mode_f) \
   DECL(maverick_et73) \
   DECL(mebus433) \
   DECL(missil_ml0757) \
   DECL(new_template) \
-  DECL(newkaku) \
-  DECL(nexa) \
   DECL(nexus) \
   DECL(nice_flor_s) \
+  DECL(oil_standard) \
+  DECL(oil_watchman) \
   DECL(opus_xt300) \
   DECL(oregon_scientific_sl109h) \
   DECL(oregon_scientific_v1) \
   DECL(philips_aj3650) \
   DECL(philips_aj7010) \
   DECL(prologue) \
-  DECL(proove) \
   DECL(quhwa) \
   DECL(rftech) \
   DECL(rubicson) \
@@ -94,11 +121,21 @@
   DECL(springfield) \
   DECL(tfa_30_3221) \
   DECL(tfa_drop_303233) \
+  DECL(tfa_marbella) \
   DECL(tfa_pool_thermometer) \
   DECL(tfa_twin_plus_303049) \
   DECL(thermopro_tp11) \
   DECL(thermopro_tp12) \
   DECL(thermopro_tx2) \
+  DECL(tpms_abarth124) \
+  DECL(tpms_citroen) \
+  DECL(tpms_elantra2012) \
+  DECL(tpms_ford) \
+  DECL(tpms_jansite) \
+  DECL(tpms_jansite_solar) \
+  DECL(tpms_pmv107j) \
+  DECL(tpms_renault) \
+  DECL(tpms_toyota) \
   DECL(ts_ft002) \
   DECL(visonic_powercode) \
   DECL(waveman) \
@@ -108,6 +145,8 @@
   DECL(wt1024) \
   DECL(X10_RF) \
   DECL(x10_sec) \
+  /* Add new decoders here. */
+#define NUMOFDEVICES 136
   /* Add new decoders here. */
 #else
 /**
--- tools/update_rtl_433_devices.sh
+++ tools/update_rtl_433_devices.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
+export MODULATION="OOK_PULSE_PWM|OOK_PULSE_PPM|FSK_PULSE_PCM"
+
 ( cd .. ; rm -rf rtl_433 ; git clone https://github.com/merbanan/rtl_433 )
 ( cd ../rtl_433/src/devices/ ; egrep "\.name|\.modulation|\.decode_fn|^r_device " *.c ) |\
-awk -f device.awk | egrep "OOK_PULSE_PWM|OOK_PULSE_PPM" | awk -F : '{ print $1 }' | sort | uniq > copy_list
+awk -f device.awk | egrep ${MODULATION} | awk -F : '{ print $1 }' | sort | uniq > copy_list
 
 # Populate src/rtl_433/device
 
@@ -20,14 +22,38 @@ done
 
 # remove non-functional device decoders
 
+# device decoders that place a bitbuffer_t on the stack is causing an overflow
+# ie bitbuffer_t databits = {0};
+
+for i in newkaku.c nexa.c proove.c
+do
+rm ../src/rtl_433/devices/$i
+done
+
 # create include/rtl_433_devices.h
 
 ( cd ../src/rtl_433/devices ; egrep "\.name|\.modulation|\.decode_fn|^r_device " *.c ) > devices.list
 
-cat devices.list | awk -f device.awk | egrep "OOK_PULSE_PWM|OOK_PULSE_PPM" | awk -F\" '{ print $3 }' | awk -F, '{ print $3 }' | awk '{ print "  DECL("$1") \\" }' > rtl_433_devices.fragment
+COUNT=`cat devices.list | awk -f device.awk | egrep ${MODULATION} | awk -F\" '{ print $3 }' | awk -F, '{ print $3 }' | wc | awk '{ print $1 }'`
+
+cat devices.list | awk -f device.awk | egrep ${MODULATION} | awk -F\" '{ print $3 }' | \
+awk -F, '{ print $3 }' | awk '{ print "  DECL("$1") \\" }' > rtl_433_devices.fragment
+
+echo "  /* Add new decoders here. */" >> rtl_433_devices.fragment
+
+echo "#define NUMOFDEVICES ${COUNT}" >> rtl_433_devices.fragment
 
 cat rtl_433_devices.pre rtl_433_devices.fragment rtl_433_devices.post > ../include/rtl_433_devices.h
 
 # create src/rtl_433_ESP.cpp fragment
 
-cat ../include/rtl_433_devices.h | grep DECL | cut -c8- | awk -F\) '{ print "memcpy(&cfg->devices["NR-1"], &"$1", sizeof(r_device));" }' > rtl_433_ESP.fragment
+echo "  // This is a generated fragment from tools/update_rtl_433_devices.sh" > rtl_433_ESP.fragment
+
+echo "" >> rtl_433_ESP.fragment
+
+cat devices.list | awk -f device.awk | egrep ${MODULATION} | awk -F\" '{ print $3 }' | \
+awk -F, '{ print $3 }' | awk '{ print "  memcpy(&cfg->devices["NR-1"], &"$1", sizeof(r_device));" }' >> rtl_433_ESP.fragment
+
+echo "" >> rtl_433_ESP.fragment
+
+echo "  // end of fragement" >> rtl_433_ESP.fragment
