13171ea3d5a75fb6a119a14becfb81fb2c19f3b1
Lucio Tarantino <lucio.tarantino@gmail.com>
2023-12-17 16:32:12 +0100

    Add Support for ESP32-C3 e S3 (#124)
    * Add Support for ESP32-C3 e S3
    
    * Update rtl_433_ESP.cpp
    
    ---------
    
    Co-authored-by: Northern Man <19808920+NorthernMan54@users.noreply.github.com>

--- src/rtl_433_ESP.cpp
+++ src/rtl_433_ESP.cpp
@@ -32,7 +32,11 @@
 #if defined(RF_MODULE_SCK) && defined(RF_MODULE_MISO) && \
     defined(RF_MODULE_MOSI) && defined(RF_MODULE_CS)
 #  include <SPI.h>
+#  if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3
+SPIClass newSPI(FSPI);
+#  else
 SPIClass newSPI(VSPI);
+#  endif
 #endif
 
 #ifdef RF_SX1276
