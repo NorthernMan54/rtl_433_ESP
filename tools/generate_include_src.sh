24faed33cd25ada7b6dcfae818b8aabc92d61088
MDW <mdeweerd@users.noreply.github.com>
2023-12-17 16:19:46 +0100

    QUAL Beautify shell scripts (#121)

--- tools/generate_include_src.sh
+++ tools/generate_include_src.sh
@@ -10,17 +10,17 @@ echo
 rm include_copy_list include_copy_and_edit_list src_copy_and_edit_list src_copy_list
 for i in `ls ../include/ | grep -v rtl_433_devices.h | grep -v "^log.h"`
 do
-  echo
-  # echo "Checking " $i
-  diff ../include/$i ../rtl_433/include/$i >> /dev/null
-  retVal=$?
-if [ $retVal -ne 0 ]; then
-    echo "File "$i "was changed"
-    echo $i >> include_copy_and_edit_list
-else
-    echo "File "$i "was not changed"
-    echo $i >> include_copy_list
-fi
+    echo
+    # echo "Checking " $i
+    diff ../include/$i ../rtl_433/include/$i >> /dev/null
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        echo "File "$i "was changed"
+        echo $i >> include_copy_and_edit_list
+    else
+        echo "File "$i "was not changed"
+        echo $i >> include_copy_list
+    fi
 done
 
 echo
@@ -32,15 +32,15 @@ ls ../src/rtl_433 | grep -v devices
 echo
 for i in `ls ../src/rtl_433 | grep -v devices`
 do
-  echo
-  echo "Checking " $i
-  diff ../src/rtl_433/$i ../rtl_433/src/$i >> /dev/null
-  retVal=$?
-if [ $retVal -ne 0 ]; then
-    echo "File "$i "was changed"
-    echo $i >> src_copy_and_edit_list
-else
-    echo "File "$i "was not changed"
-    echo $i >> src_copy_list
-fi
+    echo
+    echo "Checking " $i
+    diff ../src/rtl_433/$i ../rtl_433/src/$i >> /dev/null
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        echo "File "$i "was changed"
+        echo $i >> src_copy_and_edit_list
+    else
+        echo "File "$i "was not changed"
+        echo $i >> src_copy_list
+    fi
 done
