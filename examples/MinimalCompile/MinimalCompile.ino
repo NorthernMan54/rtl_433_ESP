24f5de020d661b6d0bc031c52a53d8141987e183
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2026-08-16 11:29:47 -0400

    Arduino

--- examples/MinimalCompile/MinimalCompile.ino
+++ examples/MinimalCompile/MinimalCompile.ino
@@ -1,5 +1,7 @@
 #include <rtl_433_ESP.h>
 
+// The CI build selects a CC1101 and its GDO pins through compiler flags. Radio
+// selection is a library-wide compile-time setting, not a sketch-local define.
 rtl_433_ESP receiver;
 
 void setup() {}
